f8d9f8daba02b7a912acba03ad9ec4027887c25a
caleb <bargercj20@hotmail.com>
2020-08-14 07:29:41 -0700

    asdf

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -3,7 +3,7 @@ from player import Player
 from world import World
 from util import Stack 
 
-import random
+import random, time
 from ast import literal_eval
 
 # Load world
@@ -12,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -37,7 +37,71 @@ r = player.current_room.id
 
 # populate the stack and graph with starting room info
 graph[r] = {k:'?' for k in player.current_room.get_exits()}
-print(graph[r])
+
+for k in graph[r].keys():
+    s.push(k)
+
+def add_way_back(d):
+    if d == 'n':
+        s.push('s')
+    if d == 's':
+        s.push('n')
+    if d == 'e':
+        s.push('w')
+    if d == 'w':
+        s.push('e')
+
+# while the stack is not empty keep exploring
+while s.size() > 0:
+    direction = s.pop()
+    # add this movement to the path
+    traversal_path.append(direction)
+    prev_room = player.current_room.id
+
+    player.travel(direction) # move the player
+
+    # ok now I need the new rooms id
+    r = player.current_room.id
+
+    # if the entry does not exist populate the graph
+    if r not in graph:
+        graph[r] = {k:'?' for k in player.current_room.get_exits()}
+
+    # update the graph for both rooms
+    graph[prev_room][direction] = r
+   
+    if direction == 'n':
+        graph[r]['s'] = prev_room
+    elif direction == 's':
+        graph[r]['n'] = prev_room
+    elif direction == 'e':
+        graph[r]['w'] = prev_room
+    else:
+        graph[r]['e'] = prev_room
+
+    # only push new direction onto the stack
+    push_count = 0
+    for d in player.current_room.get_exits():
+        if graph[r][d] == '?':
+            s.push(d)
+            push_count += 1
+            backtracking = False
+
+    if push_count == 0 and not backtracking:
+        backtracking = True
+    
+    if '?' not in graph[r].values() and not backtracking:
+        s.pop()
+        add_way_back(direction)
+        add_way_back(direction)
+
+    
+    time.sleep(3)
+    print(f"STACK - {s.stack}")
+    print(f"graph - {graph}")
+    print(f"ROOM - {player.current_room.id}")
+    
+    print("\n")
 
 # TRAVERSAL TEST
 visited_rooms = set()
